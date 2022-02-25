47cd7d216db7fa7f99005605c52eb6c536aab3a2
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2022-02-25 18:17:56 -0300

    remove db init from App

--- App.tsx
+++ App.tsx
@@ -1,23 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Database } from './src/db/Database';
+import { View } from 'react-native';
 import { ContactsPage } from './src/pages/Contact/ContactPage';
 
 export default function App() {
-
-  Database.initDb(!true)
-
   return <View>
     <StatusBar style='inverted' translucent={false}/>
     <ContactsPage />
   </View>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
